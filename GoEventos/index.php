4f5e02021b4c5596e5a79e89ecf45238b2ab7926
jesusrlv <jesusrlvrojo@gmail.com>
2022-04-27 16:06:47 -0500

    index de GoldAxs

---
+++ GoEventos/index.php
@@ -0,0 +1,117 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.88.1">
+    <title>Smart-Event · by GO Ax's</title>
+    <link rel="icon" type="image/png" href="img/eventos.ico">
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/blog/">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+    .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+    }
+
+    @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+            font-size: 3.5rem;
+        }
+    }
+    </style>
+
+
+    <!-- Custom styles for this template -->
+
+    <link href="https://fonts.googleapis.com/css?family=Playfair&#43;Display:700,900&amp;display=swap" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="blog.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+    <link href="navbar-top-fixed.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@200&display=swap" rel="stylesheet">
+
+</head>
+
+<body class="d-flex flex-column min-vh-100">
+
+    <nav class="navbar navbar-expand-md fixed-top" style="background-color: rgba(248, 249, 250, 0.7);">
+        <div class="container-fluid justify-content-center">
+            <a class="navbar-brand" style="color: #f3a79c; font-size:3rem; font-family: 'Josefin Sans', sans-serif;"
+                href="#">
+                <strong>GOLD AXs </strong></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+                aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+    </nav>
+
+    <main class="container mt-5 mb-5">
+        <div class="container mb-3">
+          <ul class="nav nav-tabs">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#"><i class="bi bi-check-circle-fill"></i> Eventos activos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#"><i class="bi bi-x-circle-fill"></i> Eventos inactivos</a>
+            </li>
+          </ul> 
+        </div>
+
+        <div class="p-4 p-md-5 mb-4 text-white rounded" style="background-color: #f7c6bf; ">
+            <div class="col-md-6 px-0">
+                <a style="font-size: 1.8rem; font-family: 'Josefin Sans', sans-serif;"><strong>Listado de eventos</strong></a>
+            </div>
+           
+            <div class="container rounded-3 p-3 mt-2" style="background-color:#7B8DAB;">
+              <div class="mt-1"><!-- inicia caja -->
+                <div class="bg-light rounded-2" style="border-top: 3px groove #1C6EA4; box-shadow: 0px 10px 13px -7px #000000, 5px 5px 3px 18px rgba(0,0,0,0);">
+                  <p class="ms-3 pt-2 text-secondary" style="color:#7B8DAB;font-size:1rem; font-family: 'Josefin Sans', sans-serif;"><strong>Nombre del evento:</strong></p>
+                  <p class="ms-3 pt-0 pb-2 text-secondary" style="color:#7B8DAB;font-size:1rem; font-family: 'Josefin Sans', sans-serif;"><strong>Fecha:</strong></p>
+                </div>
+              </div><!-- fin caja -->
+            
+              <div class="mt-2">
+                <div class="bg-light rounded-2" style="border-top: 3px groove #1C6EA4;  box-shadow: 0px 10px 13px -7px #000000, 5px 5px 3px 18px rgba(0,0,0,0);">
+                  <p class="ms-3 pt-2 text-secondary" style="color:#7B8DAB;font-size:1rem; font-family: 'Josefin Sans', sans-serif;"><strong>Nombre del evento</strong></p>
+                  <p class="ms-3 pt-0 pb-2 text-secondary" style="color:#7B8DAB;font-size:1rem; font-family: 'Josefin Sans', sans-serif;"><strong>Fecha</strong></p>
+                </div>
+              </div>
+            </div>
+        </div>
+
+
+
+    </main>
+
+    <footer class="footer mt-auto py-3 text-light" style="background-color:#7B8DAB; font-size:1.3rem; font-family: 'Josefin Sans', sans-serif;">
+        <div class="container">
+            <p>Eventos desarrollado por: <strong>Gold AXs.</strong>
+            </p>
+            <p>
+                <a href="#">Ir arriba</a>
+            </p>
+        </div>
+    </footer>
+
+</body>
+
+</html>
+

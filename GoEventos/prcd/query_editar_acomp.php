e1ab241f760b761fcd8af54d7bf53414d93f67c7
AnnyBarba <aebarbap@gmail.com>
2022-06-08 17:09:45 -0500

    Revert "actualizacion de invitados secundarios funcionando al 100"
    This reverts commit 802136592266c6c712e90127bf499283fbf57505.
    
    invitados secundarios al 100

--- GoEventos/prcd/query_editar_acomp.php
+++ GoEventos/prcd/query_editar_acomp.php
@@ -6,9 +6,8 @@
 
 <?php
     include('qconn/qc.php');
-    // $idEje = $_POST['invitado_eje'];
+    $idEje = $_POST['ideje'];
     $id = $_POST['id'];
-    $id_eje = $_POST['id_eje'];
     $idEvent = $_POST['id_evento'];
     $apellido_p = $_POST['apellido_p'];
     $apellido_m = $_POST['apellido_m'];
@@ -29,7 +28,7 @@
             title: 'Invitado actualizado',
             text: 'Los datos del invitado se han actualizado satisfactoriamente',
             footer: 'Desarrollado por: XXXX XXX XXX</a>'
-        }).then(function(){window.location='../invitados_secondary.php?id=".$id_eje."&id2=".$idEvent."';});</script>";
+        }).then(function(){window.location='../invitados_secondary.php?id=".$id."&id2=".$idEvent."';});</script>";
         }
         else{
         echo 'No se registró la actividad';
